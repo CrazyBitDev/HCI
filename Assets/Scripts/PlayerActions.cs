143ee69819b47447f679f32c72bb07bb7d5e5da7
CrazyBitDev <matteoingusci@virgilio.it>
2023-12-06 09:47:47 +0100

    hw1

--- Assets/Scripts/PlayerActions.cs
+++ Assets/Scripts/PlayerActions.cs
@@ -1,16 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerActions : MonoBehaviour
 {
+
+    public static PlayerActions Instance { get; private set; }
+
     private PlayerInput playerActionSystem;
     [SerializeField] private float movSpeed;
     [SerializeField] private float turnSpeed;
     [SerializeField] private Animator playerAnimator;
 
+    //separator on the inspector
+    [Space(10)]
+
+    [SerializeField] private float playerHeight;
+    [SerializeField] private float playerRadius;
+    [SerializeField] private float movDist;
+
     private void Awake()
     {
+        Instance = this;
         playerActionSystem = new PlayerInput();
         playerActionSystem.PlayerActions.Enable();
     }
@@ -24,23 +36,45 @@ public class PlayerActions : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         Vector2 readVec = playerActionSystem.PlayerActions.Movement.ReadValue<Vector2>();
         Vector3 movVec = new Vector3(readVec.x, 0, readVec.y);
-        this.transform.position += movVec * movSpeed * Time.deltaTime;
-        this.transform.forward = Vector3.Slerp(this.transform.forward, movVec, Time.deltaTime * turnSpeed);
-
-        /*
-         * La prof ha scritto questo, fa schifo. MI dispiace ma rimane come giù
-         * 
         
-        if (movVec.magnitude != 0)
+        bool canMove = ShootCapsule(movVec);
+
+        if (!canMove)
         {
-            playerAnimator.SetBool("isWalking", true);
-        } else
+            Vector3 movVecX = new Vector3(readVec.x, 0, 0).normalized;
+
+            canMove = ShootCapsule(movVecX);
+            if (canMove)
+            {
+                movVec = movVecX;
+            } else
+            {
+                Vector3 movVecZ = new Vector3(0, 0, readVec.y).normalized;
+
+                canMove = ShootCapsule(movVecZ);
+                if (canMove)
+                {
+                    movVec = movVecZ;
+                }
+            }
+        }
+
+        if (canMove)
         {
-            playerAnimator.SetBool("isWalking", false);
-        }*/
+            this.transform.position += movVec * movSpeed * Time.deltaTime;
+        }
+
+        this.transform.forward = Vector3.Slerp(this.transform.forward, movVec, Time.deltaTime * turnSpeed);
 
         playerAnimator.SetBool("isWalking", movVec.magnitude != 0);
     }
+
+    private bool ShootCapsule(Vector3 toVec)
+    {
+        return !Physics.CapsuleCast(this.transform.position, this.transform.position + Vector3.up * playerHeight, playerRadius, toVec, movDist);
+    }
+
 }
